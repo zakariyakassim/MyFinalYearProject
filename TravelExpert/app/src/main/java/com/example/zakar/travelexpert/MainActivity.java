929b8449094acfb8d0090f5afe59b5dea29fa51a
zakariyakassim <zakariyakassim@gmail.com>
2015-10-27 00:07:09 +0000

    Tabs working fine now

--- TravelExpert/app/src/main/java/com/example/zakar/travelexpert/MainActivity.java
+++ TravelExpert/app/src/main/java/com/example/zakar/travelexpert/MainActivity.java
@@ -18,9 +18,9 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        tabLayout.addTab(tabLayout.newTab().setText("BUS"));
+        tabLayout.addTab(tabLayout.newTab().setText("TUBE"));
+        tabLayout.addTab(tabLayout.newTab().setText("RAIL"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
--- TravelExpert/app/src/main/java/com/example/zakar/travelexpert/PagerAdapter.java
+++ TravelExpert/app/src/main/java/com/example/zakar/travelexpert/PagerAdapter.java
@@ -41,6 +41,6 @@ public class PagerAdapter extends FragmentStatePagerAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return mNumOfTabs;
     }
 }
